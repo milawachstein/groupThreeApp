7dcc33d87953d944e876043d71d5d8e1988e99bc
Mila Wachstein <mila.wachstein@gmail.com>
2024-07-26 13:00:07 -0400

    Initial Commit

---
+++ groupThreeApp/groupThreeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  groupThreeAppApp.swift
+//  groupThreeApp
+//
+//  Created by Mila Wachstein on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct groupThreeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
